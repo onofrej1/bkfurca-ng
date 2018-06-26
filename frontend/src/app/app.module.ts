d488e4ce908ce716fafd2f7f66bf66a20db30651
erik <erik.onofrej@centrum.sk>
2018-06-26 14:29:01 +0200

    commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -23,13 +23,13 @@ import { DataTableModule } from "primeng/datatable";
 import { MenubarModule } from "primeng/menubar";
 import { MenuItem } from "primeng/api";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import {
+/*import {
   removeNgStyles,
   createNewHosts,
   bootloader,
   createInputTransfer,
   hmrModule
-} from "@angularclass/hmr";
+} from "@angularclass/hmr";*/
 
 import { AdminModule } from "./admin/admin.module";
 import { PageComponent } from "./page/page.component";
@@ -77,56 +77,5 @@ const appRoutes: Routes = [
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(public appRef: ApplicationRef) {}
-  hmrOnInit(store) {
-    if (!store || !store.state) return;
-    console.log("HMR store", store);
-    console.log("store.state.data:", store.state.data);
-    // inject AppStore here and update it
-    // this.AppStore.update(store.state)
-    if ("restoreInputValues" in store) {
-      store.restoreInputValues();
-    }
-    // change detection
-    this.appRef.tick();
-    delete store.state;
-    delete store.restoreInputValues;
-  }
-  hmrOnDestroy(store) {
-    var cmpLocation = this.appRef.components.map(
-      cmp => cmp.location.nativeElement
-    );
-    // recreate elements
-    store.disposeOldHosts = createNewHosts(cmpLocation);
-    // inject your AppStore and grab state then set it on store
-    // var appState = this.AppStore.get()
-    store.state = { data: "yolo" };
-    // store.state = Object.assign({}, appState)
-    // save input values
-    store.restoreInputValues = createInputTransfer();
-    // remove styles
-    removeNgStyles();
-  }
-  hmrAfterDestroy(store) {
-    // display new elements
-    store.disposeOldHosts();
-    delete store.disposeOldHosts;
-    // anything you need done the component is removed
-  }
+  
 }
-
-export function main() {
-  return (
-    platformBrowserDynamic()
-      .bootstrapModule(AppModule)
-      // use `hmrModule` or the "@angularclass/hmr-loader"
-      .then((ngModuleRef: any) => {
-        // `module` global ref for webpackhmr
-        // Don't run this in Prod
-        return hmrModule(ngModuleRef, module);
-      })
-  );
-}
-
-// boot on document ready
-bootloader(main);
